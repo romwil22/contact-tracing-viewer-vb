d4639dc642249ad471b4380be3c3dbd17f92c161
romwil22 <romwil.pilapil@gmail.com>
2022-06-25 00:34:15 +0800

    testing app

--- contact_tracing_viewer/contact_tracing_viewer/Form1.vb
+++ contact_tracing_viewer/contact_tracing_viewer/Form1.vb
@@ -1,3 +1,16 @@
 ﻿Public Class Form1
+    Private Sub readButton_Click(sender As Object, e As EventArgs) Handles readButton.Click
+        Dim studentReader As System.IO.StreamReader
+        Dim fileLastName As String = fileNameBox.Text.ToUpper() + ".txt"
+        studentReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\Rom\Desktop\5TH-YR-2ND-SEM\elec3\contact-tracing-vb\vb_contract_tracing\vb_contract_tracing\bin\Debug\" + fileLastName)
 
+        'readTextFileRichBox.Text = studentReader.ReadToEnd()
+        readTextFileRichBox.Text = studentReader.ReadLine()
+        readTextFileRichBox.Text += vbNewLine + studentReader.ReadLine()
+        readTextFileRichBox.Text += vbNewLine + studentReader.ReadLine()
+        readTextFileRichBox.Text += vbNewLine + studentReader.ReadLine()
+        readTextFileRichBox.Text += vbNewLine + studentReader.ReadLine()
+        readTextFileRichBox.Text += vbNewLine + studentReader.ReadLine()
+
+    End Sub
 End Class
